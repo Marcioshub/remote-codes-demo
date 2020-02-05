381c52df4dede95baf3cef4d10288e5a07f45a19
Marcio Castillo <mecastillo00@gmail.com>
2020-02-05 16:41:49 -0500

    fixed footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import "./App.css";
 import Navigationbar from "./components/Navigationbar";
 import Typography from "@material-ui/core/Typography";
 import Home from "./Home";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  font: {
+    fontSize: "1.5vw"
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div style={{ position: "relative", textAlign: "center", height: "100vh" }}>
       <Navigationbar />
@@ -21,7 +29,12 @@ function App() {
           width: "100%"
         }}
       >
-        <Typography variant="overline" display="block" gutterBottom>
+        <Typography
+          variant="overline"
+          display="block"
+          gutterBottom
+          className={classes.font}
+        >
           Beam® is a registered trademark of Southern Telecom, Inc.
         </Typography>
       </div>
